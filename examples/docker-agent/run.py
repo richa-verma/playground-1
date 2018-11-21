87d787c19e555cd709bb1d222b70d20cd26dfaf3
richa-verma <richa15054@iiitd.ac.in>
2018-11-21 17:48:45 +0530

    Update run.py

--- examples/docker-agent/run.py
+++ examples/docker-agent/run.py
@@ -8,7 +8,7 @@ class MyAgent(DockerAgentRunner):
     '''An example Docker agent class'''
 
     def __init__(self):
-        self._agent = agents.SimpleAgent()
+        self._agent = agents.RL_learned_agent()
 
     def init_agent(self, id, game_type):
         return self._agent.init_agent(id, game_type)
